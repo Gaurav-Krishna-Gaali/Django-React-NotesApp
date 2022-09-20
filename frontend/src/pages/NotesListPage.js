75f013fc83d212f97209dbac776aa37186d63226
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-09-21 00:47:29 +0530

    React setup 🙃💯

---
+++ frontend/src/pages/NotesListPage.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const NotesListPage = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {}, []);
+
+  let getNotes = async () => {};
+
+  return <div>NotesListPage</div>;
+};
+
+export default NotesListPage;
