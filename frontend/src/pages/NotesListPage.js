fa3c456c8769e2aab3a402ad2c08c364023feddc
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-09-22 00:57:44 +0530

    Routing done foe the note elements ��

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from "react";
+import ListItem from "../components/ListItem";
 
 const NotesListPage = () => {
   const [notes, setNotes] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getNotes();
+  }, []);
 
-  let getNotes = async () => {};
+  let getNotes = async () => {
+    let response = await fetch("/api/notes/");
+    let data = await response.json();
+    console.log("DATA:", data);
+    setNotes(data);
+  };
 
-  return <div>NotesListPage</div>;
+  return (
+    <div>
+      <div className="notes-list">
+        {notes.map((note, index) => (
+          <ListItem note={note} key={index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default NotesListPage;
