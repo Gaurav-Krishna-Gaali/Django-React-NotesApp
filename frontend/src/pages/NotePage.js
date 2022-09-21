28de5fbcab2320cff8e136759796baecc1a9f429
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-09-22 01:22:30 +0530

    Styled Components 💅💅

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { ReactComponent as Arrowleft } from "../assets/arrow-left.svg";
 
 const NotePage = ({ match }) => {
   let { id: noteId } = useParams();
@@ -17,8 +18,15 @@ const NotePage = ({ match }) => {
   };
 
   return (
-    <div>
-      <p>{note?.body}</p>
+    <div className="note">
+      <div className="note-header">
+        <h3>
+          <Link to="/">
+            <Arrowleft />
+          </Link>
+        </h3>
+      </div>
+      <textarea defaultValue={note?.body}></textarea>
     </div>
   );
 };
