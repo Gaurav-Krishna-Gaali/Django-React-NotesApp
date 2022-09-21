33b9c59902579134431877c8f53430aaaa5f8bf8
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-09-22 01:19:47 +0530

    Styled the components ��

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<NotesListPage />} />
-          <Route path="note/:id" element={<NotePage />} />
-        </Routes>
+      <div className="container dark">
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<NotesListPage />} />
+            <Route path="note/:id" element={<NotePage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
